2821f6abd24c9a8835cf20cde3b2b5e01034bda6
glosona <sonaze28@gmail.com>
2025-08-07 16:26:28 +0900

    3. Spring boot API

--- comentoStatistic/src/main/java/com/demo/comentoStatistic/controller/PingController.java
+++ comentoStatistic/src/main/java/com/demo/comentoStatistic/controller/PingController.java
@@ -1,4 +1,4 @@
-package com.demo.devfun.controller;
+package com.demo.comentoStatistic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
